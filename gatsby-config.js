14032016959a9f250049f52f4924fc6929fdc2e6
SL4YT4NIC <tldcloudmr@gmail.com>
2024-01-19 13:32:47 -0700

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,12 +3,19 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `TLD_BLOG2`,
+    title: `TRADE LEVEL DAILY`,
     siteUrl: `https://www.yourdomain.tld`
   },
   plugins: [
     "gatsby-plugin-netlify-cms",
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
   ],
 };
